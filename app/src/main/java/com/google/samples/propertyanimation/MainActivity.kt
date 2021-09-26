f394ab9a7ef36cfa06563b848036bdce08feacb7
matheus-miranda <matheus.sm86@gmail.com>
2021-09-26 19:12:30 -0300

    Implement translate function and create extension function to disable button during animation

--- app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -42,48 +42,60 @@ class MainActivity : AppCompatActivity() {
             rotate()
         }
 
-        /**
         binding.translateButton.setOnClickListener {
-        translate()
+            translate()
         }
 
+
         binding.scaleButton.setOnClickListener {
-        scale()
+            scale()
         }
 
         binding.fadeButton.setOnClickListener {
-        fade()
+            fade()
         }
 
         binding.colorizeButton.setOnClickListener {
-        colorize()
+            colorize()
         }
 
         binding.showerButton.setOnClickListener {
-        shower()
+            shower()
         }
-         */
+    }
+
+    /**
+     * Extension function to disable and enable view while animation is running to prevent jank
+     */
+    private fun ObjectAnimator.disableViewDuringAnimation(view: View) {
+        this.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator?) {
+                view.isEnabled = false
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                view.isEnabled = true
+            }
+        })
     }
 
     private fun rotate() {
         // Animation should end with the view being at its default state, hence starting at -360
         val animator = ObjectAnimator.ofFloat(binding.star, View.ROTATION, -360f, 0f)
         animator.duration = 2000 // default is 300ms
-
-        // Listener to disable and enable button while animation is running to prevent jank
-        animator.addListener(object : AnimatorListenerAdapter() {
-            override fun onAnimationStart(animation: Animator?) {
-                binding.rotateButton.isEnabled = false
-            }
-            override fun onAnimationEnd(animation: Animator?) {
-                binding.rotateButton.isEnabled = true
-            }
-        })
-
+        animator.disableViewDuringAnimation(binding.rotateButton)
         animator.start()
     }
 
+    /**
+     * Moves the star horizontally
+     */
     private fun translate() {
+        val animator = ObjectAnimator.ofFloat(binding.star, View.TRANSLATION_X, 200f)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(binding.translateButton)
+        animator.start()
     }
 
     private fun scale() {
