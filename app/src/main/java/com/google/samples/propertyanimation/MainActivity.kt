8eb2f0a89bd7f5796ce56f3613f2b02c5f21b519
matheus-miranda <matheus.sm86@gmail.com>
2021-09-26 23:43:25 -0300

    Implement shower star function

--- app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -16,15 +16,17 @@
 
 package com.google.samples.propertyanimation
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.ObjectAnimator
-import android.animation.PropertyValuesHolder
+import android.animation.*
 import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.LinearInterpolator
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
 import com.google.samples.propertyanimation.databinding.ActivityMainBinding
 
 
@@ -143,6 +145,63 @@ class MainActivity : AppCompatActivity() {
         animator.start()
     }
 
+    /**
+     * Create a new star view in a random X position above the container.
+     * Make it rotateButton about its center as it falls to the bottom.
+     */
     private fun shower() {
+        // Local variables we'll need in the code below
+        val container = binding.star.parent as ViewGroup // is where we'll add the stars
+        val containerW = container.width
+        val containerH = container.height
+        var starW: Float = binding.star.width.toFloat()
+        var starH: Float = binding.star.height.toFloat()
+
+        // Create the new star (an ImageView holding our drawable) and add it to the container
+        val newStar = AppCompatImageView(this)
+        newStar.setImageResource(R.drawable.ic_star)
+        newStar.layoutParams = FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.WRAP_CONTENT,
+            FrameLayout.LayoutParams.WRAP_CONTENT
+        )
+        container.addView(newStar)
+
+        // Scale the view randomly between 10-160% of its default size
+        newStar.scaleX = Math.random().toFloat() * 1.5f + .1f
+        newStar.scaleY = newStar.scaleX
+        starW *= newStar.scaleX
+        starH *= newStar.scaleY
+
+        // Position the view at a random place between the left and right edges of the container
+        newStar.translationX = Math.random().toFloat() * containerW - starW / 2
+
+        // Create an animator that moves the view from a starting position right about the container
+        // to an ending position right below the container. Set an accelerate interpolator to give
+        // it a gravity/falling feel. This is the falling animation
+        val mover = ObjectAnimator.ofFloat(newStar, View.TRANSLATION_Y, -starH, containerH + starH)
+        mover.interpolator = AccelerateInterpolator(1f)
+
+        // Create an animator to rotateButton the view around its center up to three times
+        val rotator = ObjectAnimator.ofFloat(
+            newStar, View.ROTATION,
+            (Math.random() * 1080).toFloat()
+        )
+        rotator.interpolator = LinearInterpolator()
+
+        // Use an AnimatorSet to play the falling and rotating animators in parallel for a duration
+        // of a half-second to two seconds
+        val set = AnimatorSet()
+        set.playTogether(mover, rotator)
+        set.duration = (Math.random() * 1500 + 500).toLong()
+
+        // When the animation is done, remove the created view from the container
+        set.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                container.removeView(newStar)
+            }
+        })
+
+        // Start the animation
+        set.start()
     }
 }
